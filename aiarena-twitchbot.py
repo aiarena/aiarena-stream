74f1a6b71b9061262fdbd37c909316dd73c1b015
lladdy <randallboland@bigpond.com>
2019-11-07 03:12:08 +1030

    Add Twitch chat match queuing

--- aiarena-twitchbot.py
+++ aiarena-twitchbot.py
@@ -4,6 +4,13 @@ from config import irc_token, client_id
 
 # api token can be passed as test if not needed.
 # Channels is the initial channels to join, this could be a list, tuple or callable
+from util import queue_file
+
+def queue_match(match_id):
+    f = open(queue_file, "a+")
+    f.write(str(match_id) + "\n")
+    f.close()
+
 bot = commands.Bot(
     irc_token=irc_token,
     client_id=client_id,
@@ -34,5 +41,23 @@ async def next_command(ctx):
         await ctx.send(f'Okay {ctx.author.name} - I will restart Sc2! - Please wait')
         os.system("taskkill /f /im SC2_x64.exe")
         os.system("taskkill /f /im ExampleObserver.exe")
+
+
+@bot.command(name='queue', aliases=['q'])
+async def queue_command(ctx):
+    if ctx.author.is_mod:
+        match_id = None
+
+        try:
+            if ctx.content[:7] == '!queue ':
+                match_id = int(ctx.content[7:])
+            elif ctx.content[:3] == '!q ':
+                match_id = int(ctx.content[3:])
+        except ValueError:
+            await ctx.send(f'Sorry {ctx.author.name}, please supply a valid match id.')
+
+        if match_id is not None:
+            queue_match(match_id)
+            await ctx.send(f'Match ID {match_id} queued')
 
 bot.run()
---
+++ util.py
@@ -0,0 +1,1 @@
+queue_file = "queue.txt"
