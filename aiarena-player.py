cc7711d8e5fd8556e0c994be6ef6629dbdb1d40d
lladdy <randallboland@bigpond.com>
2020-10-07 23:33:43 +1030

    Rename sc2 executable var and add data version

--- aiarena-player.py
+++ aiarena-player.py
@@ -82,9 +82,11 @@ def startbattle():
     os.system(removechatcmd)
 
     # run Observer
-    cmd = "ExampleObserver.exe --Path \"" + replaysave + "\""
-    if hasattr(config, 'sc2path') and config.sc2path is not None:
-        cmd += f" -e {config.sc2path}"
+    cmd = "ExampleObserver.exe -p \"" + replaysave + "\""
+    if hasattr(config, 'sc2_executable') and config.sc2_executable is not None:
+        cmd += f" -e {config.sc2_executable}"
+    if hasattr(config, 'sc2_data_version') and config.sc2_data_version is not None:
+        cmd += f" -d {config.sc2_data_version}"
     print("Running command:\n" + cmd)
     os.system(cmd)
     # os.system("ExampleObserver.exe --Path \"" + replaysave + "\" --data_version B89B5D6FA7CBF6452E721311BFBC6CB2")
