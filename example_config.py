87c6f20f494079902fba8354e19d0fd12213d06b
lladdy <randallboland@bigpond.com>
2021-07-12 16:39:08 +0930

    Add example config

---
+++ example_config.py
@@ -0,0 +1,7 @@
+irc_token = 'redacted'
+client_id = 'redacted'
+token = 'redacted'
+trusted_users = ['lladdy']
+sc2_executable = 'C:\Program Files (x86)\StarCraft II\Versions\Base75689\SC2_x64.exe'
+sc2_maps_folder = 'C:\Program Files (x86)\StarCraft II\Maps'
+sc2_data_version = 'B89B5D6FA7CBF6452E721311BFBC6CB2'
